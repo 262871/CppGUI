a4454db258180bb393ad08323f83aee99869e71c
user <61919302+user@users.noreply.github.com>
2022-07-05 23:41:56 +0200

    Created a simple prototype and updated the build system
    Added glfw vulkan and fmt libraries to the build

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     dir = pathlib.Path('.')
     name = dir.absolute().name
     cpp = [x.absolute() for x in dir.joinpath('src').glob('**/*.cpp')]
+    c = ['C:\\src\\volk\\volk.c']
     dir.joinpath('build').mkdir(exist_ok=True)
     out = ['-o', dir.joinpath('build', name + '_mingw64.exe').absolute()]
     options = [
@@ -15,4 +16,24 @@ if __name__ == "__main__":
         '-pedantic',
         '-O3'
     ]
-    subprocess.run(['g++'] + options + cpp + out)
+    includes = [
+        "-IC:\\src\\fmt-8.1.1\\include",
+        '-IC:\\src\\VulkanSDK\\Include',
+        '-IC:\\src\\glfw-3.3.7.bin.WIN64\\include',
+        '-IC:\\src\\volk'
+    ]
+    link = [
+        '-LC:\\src\\glfw-3.3.7.bin.WIN64\\lib-mingw-w64',
+        '-lglfw3',
+        '-lkernel32',
+        '-luser32',
+        '-lgdi32',
+        '-lwinspool',
+        '-lshell32',
+        '-lole32',
+        '-loleaut32',
+        '-luuid',
+        '-lcomdlg32',
+        '-ladvapi32',
+    ]
+    subprocess.run(['g++'] + options + includes + cpp + c + link + out)
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,55 @@
+#include <volk.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#define FMT_HEADER_ONLY
+#include <fmt/format.h>
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+     glfwInit();
+     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+     auto window = glfwCreateWindow(800, 600, "Testing glfw and vulkan", nullptr, nullptr);
+
+     if (volkInitialize() != VK_SUCCESS)
+          return 1;
+
+     uint32_t extensions {};
+     uint32_t version {};
+     vkEnumerateInstanceExtensionProperties(nullptr, &extensions, nullptr);
+     vkEnumerateInstanceVersion(&version);
+     fmt::print("{} extensions supported\nVulkan version: {}", extensions, version);
+
+     VkApplicationInfo application_info {
+          .sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+          .pNext              = nullptr,
+          .pApplicationName   = "GUI",
+          .applicationVersion = 1,
+          .pEngineName        = "CppGUI",
+          .engineVersion      = 1,
+          .apiVersion         = version
+     };
+     VkInstanceCreateInfo instance_create_info {
+          .sType                   = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+          .pNext                   = nullptr,
+          .flags                   = {},
+          .pApplicationInfo        = &application_info,
+          .enabledLayerCount       = 0,
+          .ppEnabledLayerNames     = nullptr,
+          .enabledExtensionCount   = 0,
+          .ppEnabledExtensionNames = nullptr
+     };
+     VkInstance instance {};
+     if (vkCreateInstance(&instance_create_info, nullptr, &instance) != VK_SUCCESS)
+          return 2;
+
+     while (!glfwWindowShouldClose(window)) {
+          glfwPollEvents();
+     }
+
+     vkDestroyInstance(instance, nullptr);
+     glfwDestroyWindow(window);
+     glfwTerminate();
+
      return 0;
 }
